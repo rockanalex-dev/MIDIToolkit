c470ddc6540fdbf4f4c027ba724d09f1d796bcec
Alex <alexlev61@proton.me>
2026-03-02 16:24:09 -0800

    Update MIDIToolkit.py

--- MIDIToolkit.py
+++ MIDIToolkit.py
@@ -1,3 +1,56 @@
+#! /usr/bin/python3
+
+r'''###############################################################################
+###################################################################################
+#
+#   MIDIToolkit
+#
+#	Based upon MIDI.py module v.6.7. by Peter Billam / pjb.com.au
+#
+#	Project Los Angeles
+#
+#	Tegridy Code 2026
+#
+#   https://github.com/Tegridy-Code/Project-Los-Angeles
+#
+###################################################################################
+###################################################################################
+#   Copyright 2026 Project Los Angeles / Tegridy Code
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+###################################################################################
+###################################################################################
+#
+#	PARTIAL MIDI.py Module v.6.7. by Peter Billam
+#   Please see TMIDI 2.3/tegridy-tools repo for full MIDI.py module code
+# 
+#   Or you can always download the latest full version from:
+#
+#   https://pjb.com.au/
+#   https://peterbillam.gitlab.io/miditools/
+#	
+#	Copyright 2020 Peter Billam
+#
+###################################################################################
+###################################################################################
+'''
+
+###################################################################################
+
+__version__ = "26.3.2"
+
+###################################################################################
+
 """
 Enhanced MIDI toolkit built on top of MIDI.py
 
@@ -931,4 +984,5 @@ class MIDIFile:
         return f"MIDIFile(path={self.path!r}, lazy={self.lazy})"
 
     def __str__(self) -> str:
+
         return self.info()
